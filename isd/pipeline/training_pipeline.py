e3539fa3b911e6e699054e7c207f0a1590af052f
patelshehbaz <patelshehbaz27@gmail.com>
2024-03-27 03:29:34 -0400

    model trainer added

--- isd/pipeline/training_pipeline.py
+++ isd/pipeline/training_pipeline.py
@@ -1,17 +1,31 @@
 import sys, os
 from isd.logger import logging
 from isd.exception import isdException
-from isd.components.data_ingestion import DataIngestion
 from isd.configuration.s3_operations import S3Operation
+from isd.components.data_ingestion import DataIngestion
 from isd.components.data_validation import DataValidation
+from isd.components.model_trainer import ModelTrainer
+from isd.components.model_pusher import ModelPusher
 
-from isd.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
-from isd.entity.artifacts_entity import (DataIngestionArtifact,DataValidationArtifact)
+
+from isd.entity.config_entity import (DataIngestionConfig,
+                                      DataValidationConfig,
+                                      ModelTrainerConfig,
+                                      ModelPusherConfig)
+
+
+from isd.entity.artifacts_entity import (DataIngestionArtifact,
+                                         DataValidationArtifact,
+                                         ModelTrainerArtifact,
+                                         ModelPusherArtifacts)
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model_pusher_config = ModelPusherConfig()
+        self.s3_operations = S3Operation()
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
         try: 
@@ -58,6 +72,33 @@ class TrainPipeline:
 
         except Exception as e:
             raise isdException(e, sys) from e
+    
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config,
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise isdException(e, sys)
+        
+    def start_model_pusher(self, model_trainer_artifact: ModelTrainerArtifact, s3: S3Operation):
+
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.model_pusher_config,
+                model_trainer_artifact= model_trainer_artifact,
+                s3=s3
+                
+            )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise isdException(e, sys)
+    
         
         
     def run_pipeline(self) -> None:
@@ -66,6 +107,13 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+                model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
+            
+            
+            else:
+                raise Exception("Your data is not in correct format")
 
         except Exception as e:
             raise isdException(e, sys)
